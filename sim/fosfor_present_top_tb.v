10c051f30d6ced81de1c4dc98087136bfbfaa764
Fosfor <fosfor.software@seznam.cz>
2022-11-30 04:15:54 +0100

    added testbench skeleton and top level break-out wires

---
+++ sim/fosfor_present_top_tb.v
@@ -0,0 +1,33 @@
+`default_nettype none
+`timescale 1ns/1ps
+
+module fosfor_present_top_tb;
+
+  initial begin
+    $dumpfile ("fosfor_present_top_tb.vcd");
+    $dumpvars (0, fosfor_present_top_tb);
+    #500;
+    $finish();
+  end
+
+  reg Clk_k;
+  reg Reset_r;
+  
+  initial begin
+    Clk_k = 0;
+    Reset_r = 1;
+    #10 Reset_r = 0;
+    forever 
+       #5 Clk_k = ~Clk_k;
+  end
+
+  wire [7:0] inputs = {6'b0, Reset_r, Clk_k};
+  wire [7:0] outputs;
+
+  fosfor_present_top dut_i (
+    .io_in (inputs),
+    .io_out (outputs)
+  );
+
+endmodule
+
--- src/fosfor_present_top.v
+++ src/fosfor_present_top.v
@@ -3,6 +3,30 @@ module fosfor_present_top (
   output [7:0] io_out
 );
 
-assign io_out = io_in;
+  wire Clk_ik;
+  wire Reset_ir;
+  wire [1:0] Addr_ib;
+  wire [3:0] Data_ib;
+  wire [7:0] Data_ob;
+
+  assign Clk_ik = io_in[0];
+  assign Reset_ir = io_in[1];
+  assign Addr_ib = io_in[3:2];
+  assign Data_ib = io_in[7:4];
+
+  assign io_out = Data_ob;
+  
+  /* address:
+      00 - no write, output status
+      01 - write to command register, output status
+      10 - write to input_data[3:0] register, output output_data register
+      11 - write to input_data[7:4] register, output output_data register
+   */
+   
+  wire [7:0] Status_b;
+  wire [7:0] InputData_b;
+  wire [7:0] OutputData_b;
+  
+  
 
 endmodule
